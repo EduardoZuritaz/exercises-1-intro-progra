0a09e7f94083da341c760c3dcbca2969ec8c5f0a
EduardoZuritaz <159577059+EduardoZuritaz@users.noreply.github.com>
2024-03-15 21:43:16 -0400

    prueba 2.3

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -17,7 +17,7 @@ void exercise_1(string s1, string s2, string s3) {
 
 void exercise_2(double A, double B, double C) {
   // TODO: YOUR CODE HERE
-  double discr == B*B - 4*A*C;
+  double discr = = B*B - 4*A*C;
   if (discr > 0) {
     double root1 = (-B + sqrt(discr)) / (2*A);
     double root2 = (-B - sqrt(discr)) / (2*A);
