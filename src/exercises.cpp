1a312f10a60e9d704ea61da12934daa486db1203
EduardoZuritaz <159577059+EduardoZuritaz@users.noreply.github.com>
2024-03-15 23:53:51 -0400

    prueba16.3

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -119,23 +119,22 @@ void exercise_16(int debut, int fin) {
     
     if (horaInicio < 0 || horaInicio > 24 || horaFin < 0 || horaFin > 24) {
         cout << "Las horas deben estar entre 0 y 24!" << endl;
-        return 0;
+        return ;
     }
     
     if (horaInicio == horaFin) {
         cout << "Que extraño, no has alquilado tu bicicleta por mucho tiempo!" << endl;
-        return 0;
+        return ;
     }
     
     if (horaInicio > horaFin) {
         cout << "Que extraño, el inicio del alquiler es después del final..." << endl;
-        return 0;
+        return ;
     }
     
     int horasTarifa1 = 0, horasTarifa2 = 0;
     int costoTotal = 0;
     
-    // Calcular horas en cada tarifa
     for (int i = horaInicio; i < horaFin; ++i) {
         if ((i >= 0 && i < 7) || (i >= 17 && i < 24)) {
             horasTarifa1++;
@@ -144,15 +143,13 @@ void exercise_16(int debut, int fin) {
         }
     }
     
-    // Calcular costo total
     costoTotal = horasTarifa1 + 2 * horasTarifa2;
     
-    // Mostrar resultados
     cout << "Has alquilado una bicicleta por" << endl;
     cout << horasTarifa1 << " hora(s) con el tarifario de 1 boliviano(s)" << endl;
     cout << horasTarifa2 << " hora(s) con el tarifario de 2 boliviano(s)" << endl;
     cout << "El monto total a pagar es de " << costoTotal << " boliviano(s)." << endl;
     
-    return 0;
+    return ;
 
 }
