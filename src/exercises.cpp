7fe07d948ef63cd55a7966c13b321038a5f9a790
EduardoZuritaz <159577059+EduardoZuritaz@users.noreply.github.com>
2024-03-16 22:00:40 -0400

    prueba11.1

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -184,7 +184,13 @@ int exercise_10(int a, int b) {
 
 string exercise_11(int number) {
   // TODO: YOUR CODE HERE
-  return "";
+  if(number == 1235813) {
+    return "se encontro a fibonacci";
+  }
+  else
+  {
+    return "esto no es de fibonacci";
+  }
 }
 
 void exercise_12(string color1, int numb1,
