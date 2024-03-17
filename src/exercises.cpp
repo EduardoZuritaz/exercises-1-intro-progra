85733101ec5ea3d92d53c15e96651b48bf2fcb8f
EduardoZuritaz <159577059+EduardoZuritaz@users.noreply.github.com>
2024-03-16 20:22:05 -0400

    pruebadef6

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -88,33 +88,30 @@ void exercise_5(char character) {
 
 void exercise_6(int number) {
   // TODO: YOUR CODE HERE
-      switch(number == 1){
-        cout << "Monday" << endl;
-      }
-      switch(number == 2){
-        cout << "Tuesday" << endl;
-      }
-      switch(number == 3){
-        cout << "Wednesday" << endl;
-      }
-      switch(number == 4){
-        cout << "Thursday" << endl;
-      }
-      switch(number == 5){
-        cout << "Friday" << endl;
-      }
-      switch(number == 6){
-        cout << "Saturday" << endl;
-      }
-      switch(number == 7){
-        cout << "Sunday" << endl;
-      }
-      switch(number > 7){
-        cout << "Invalid input" << endl;
-      }
-      switch(number < 1){
-        cout << "Invalid input" << endl;
-      }
+if(a == 1){
+ cout << "Monday" << endl;
+}
+else if(a== 2){
+   cout << "Tuesday" << endl;
+}
+else if(a== 3) {
+   cout << "Wednesday" << endl;
+}
+else if(a== 4) {
+   cout << "Thursday"<< endl;
+}
+else if(a== 5){
+   cout << "Friday" << endl;
+}
+else if(a== 6){
+   cout << "Saturday" << endl;
+}
+else if(a== 7){
+   cout << "Sunday" << endl;
+}
+else{
+    cout << "Invalid input" << endl;
+}
 }
 
 void exercise_7(double r) {
